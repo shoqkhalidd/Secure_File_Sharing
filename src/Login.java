c269de250906207ef8f58c64c4c544d1b3f89335
ReemaAli1 <95322384+ReemaAli1@users.noreply.github.com>
2022-04-09 17:30:56 +0300

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -1,3 +1,6 @@
+import java.security.NoSuchAlgorithmException;  
+import java.security.MessageDigest;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -226,6 +229,40 @@ public class Login extends javax.swing.JFrame {
 
     private void jPasswordField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField1ActionPerformed
         // TODO add your handling code here:
+        
+        String password = password1.getText();  
+        String encryptedpassword = null;  
+        try   
+        {  
+            /* MessageDigest instance for MD5. */  
+            MessageDigest m = MessageDigest.getInstance("MD5");  
+              
+            /* Add plain-text password bytes to digest using MD5 update() method. */  
+            m.update(password.getBytes());  
+              
+            /* Convert the hash value into bytes */   
+            byte[] bytes = m.digest();  
+              
+            /* The bytes array has bytes in decimal form. Converting it into hexadecimal format. */  
+            StringBuilder s = new StringBuilder();  
+            for(int i=0; i< bytes.length ;i++)  
+            {  
+                s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));  
+            }  
+              
+            /* Complete hashed password in hexadecimal format */  
+            encryptedpassword = s.toString();  
+        }   
+        catch (NoSuchAlgorithmException e)   
+        {  
+            e.printStackTrace();  
+        }  
+          
+        /* Display the unencrypted and encrypted passwords. */  
+       // System.out.println("Plain-text password: " + password);  
+       // System.out.println("Encrypted password using MD5: " + encryptedpassword);  
+        
+        
     }//GEN-LAST:event_jPasswordField1ActionPerformed
 
     private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
