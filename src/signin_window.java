66c91ff2f5466e7fbd3d0a7d69b2b6d154bd8438
ReemaAli1 <95322384+ReemaAli1@users.noreply.github.com>
2022-04-09 17:25:51 +0300

    Update signin_window.java

--- src/signin_window.java
+++ src/signin_window.java
@@ -6,6 +6,9 @@ import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileSystemView;
 
+import java.security.NoSuchAlgorithmException;  
+import java.security.MessageDigest;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -295,6 +298,42 @@ public class signin_window extends javax.swing.JFrame {
 
     private void UsernameFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_UsernameFocusGained
     Username.setText("");        // TODO add your handling code here:
+	    
+	String password = password2.getText();  
+        String encryptedpassword = null;  
+        try   
+        {  
+            /* MessageDigest instance for MD5. */  
+            MessageDigest m = MessageDigest.getInstance("MD5");  
+              
+            /* Add plain-text password bytes to digest using MD5 update() method. */  
+            m.update(password.getBytes());  
+              
+            /* Convert the hash value into bytes */   
+            byte[] bytes = m.digest();  
+              
+            /* The bytes array has bytes in decimal form. Converting it into hexadecimal format. */  
+            StringBuilder s = new StringBuilder();  
+            for(int i=0; i< bytes.length ;i++)  
+            {  
+                s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));  
+            }  
+              
+            /* Complete hashed password in hexadecimal format */  
+            encryptedpassword = s.toString();  
+        }   
+        catch (NoSuchAlgorithmException e)   
+        {  
+            e.printStackTrace();  
+        }  
+          
+        /* Display the unencrypted and encrypted passwords. */  
+       // System.out.println("Plain-text password: " + password);  
+       // System.out.println("Encrypted password using MD5: " + encryptedpassword);  
+        
+	    
+	       
+	    
     }//GEN-LAST:event_UsernameFocusGained
 
     /**
